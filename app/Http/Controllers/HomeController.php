048c5f48ffeabb0d6e4c63b8717a8331fd4a3014
Umut Derebek <umut.derebek@hbv.edu.tr>
2023-05-05 18:53:33 +0300

    last comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return inertia('Index/Index');
+
+        return inertia('Index/Index',
+        [
+            'name' => 'John Doe',
+            'age' => 25,
+            'hobbies' => ['Reading', 'Walking', 'Coding']
+        ]);
     }
 
     public function show()
--- resources/js/pages/Index/Index.vue
+++ resources/js/pages/Index/Index.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>Index</div>
+    <div>hayda</div>
     <Link href="/hello">Show Page</Link>
 </template>
 
 <script setup>
+defineProps(['name'])
 import { Link } from '@inertiajs/inertia-vue3'
 </script>
