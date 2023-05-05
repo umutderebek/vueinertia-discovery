f708b4bafd82972cc1382ebc7e9003f2981985a6
Umut Derebek <umut.derebek@hbv.edu.tr>
2023-05-05 18:46:51 +0300

    Initial commit

---
+++ resources/js/pages/Index/show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Index</h1>
+        <Link href="/">MainPage</Link>
+    </div>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/inertia-vue3";
+</script>
